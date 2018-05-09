a58e32a2212094e5eb2f821b3fe1135c7dc0e777
shadow <yangqiu0928@126.com>
2018-05-09 11:35:57 +0800

    add test function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,4 +71,7 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :passsword, :phone, :age, :gender)
     end
+    def test
+
+    end
 end
